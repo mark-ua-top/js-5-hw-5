db1107e266920ce4c82399e7d1ed7f2fe8d42b91
mark-ua-top <mark.d.nahorniak@gmail.com>
2025-10-12 11:42:26 +0300

    gg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import reset from './reset.css';
+import style from "./index.css"
 import './App.css';
 
 const List = styled.ul`
@@ -7,30 +9,84 @@ const List = styled.ul`
   display: flex;
   flex-direction: row;
   gap: 30px;
+  align-items: center;
+  justify-content: center;
+`;
+const ListDif = styled.ul`
+  list-style: none;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  gap: 11px;
+  align-items: center;
+  justify-content: center;
 `;
 const Item = styled.li`
-    width: 250px;
+
 `;
-const Img = styled.img`
+
+const ItemDif = styled.li`
+background-color: rgb(255, 248, 148);
+padding: 5px;
+padding-left: 10px;
+padding-right: 10px;
+border-radius: 10px;
+`;
+
+const ItemDifActive = styled.li`
+background-color: rgba(255, 76, 49, 1);
+padding: 5px;
+padding-left: 10px;
+padding-right: 10px;
+border-radius: 10px;
+`;
+
+const DivParam = styled.div`
+margin-bottom: 10px;
+background-color: #f8f8f8;
+border-radius: 25px;
+padding-top: 10px;
+padding-bottom: 10px;
+`;
+
+const DivDif = styled.div`
+background-color: #f8f8f8;
+border-radius: 25px;
+padding-top: 10px;
+padding-bottom: 10px;
+width: 200px;
+margin-left: auto;
+margin-right: auto;
+`;
+
+const Image = styled.img`
   width: 250px;
   height: 250px;
   object-fit: cover;
   border-radius: 20px;
 `;
 
-const H3 = styled.h3`
+const Title = styled.h1`
   font-size: 15px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  color: #333; 
 `;
 
-const P = styled.p`
-  font-size: 1em;
-  color: #333; 
+const SubTitle = styled.h2`
+  font-size: 15px;
+  margin-right: auto;
+    max-width: 100px;
+    margin-bottom: 10px;
   `
 
-const Div = styled.div`
-background-color: #f8f8f8;
-border-radius: 25px;
-padding: 20px;
+const Des = styled.p`
+  font-size: 10px;
+  color: #333; 
+  `
+const DesActive = styled.p`
+  font-size: 10px;
+  color: #ffffffff; 
   `
 
 function App() {
@@ -38,24 +94,24 @@ function App() {
     <div className="App">
       <List>
         <Item>
-          <Img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdfbJq5pbaqcTfA8fU_8fH1CP5EPGvJt7EPQ&s"
+          <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdfbJq5pbaqcTfA8fU_8fH1CP5EPGvJt7EPQ&s"
             alt="burger" />
-          <H3>smoked salmon burger</H3>
-          <Div>
+          <Title>Smoked salmon burger</Title>
+          <DivParam>
             <List>
-              <Item><P>20 min</P></Item>
-              <Item><P>6 servings</P></Item>
-              <Item><P>210 calories</P></Item>
+              <Item><Des>20 min</Des></Item>
+              <Item><Des>6 servings</Des></Item>
+              <Item><Des>210 calories</Des></Item>
             </List>
-          </Div>
-          <Div>
-            <H3>Difficulty</H3>
-            <List>
-              <Item><P>Easy</P></Item>
-              <Item><P>Medium</P></Item>
-              <Item><P>Hard</P></Item>
-            </List>
-          </Div>
+          </DivParam>
+          <DivDif>
+            <SubTitle>Difficulty</SubTitle>
+            <ListDif>
+              <ItemDifActive><DesActive>Easy</DesActive></ItemDifActive>
+              <ItemDif><Des>Medium</Des></ItemDif>
+              <ItemDif><Des>Hard</Des></ItemDif>
+            </ListDif>
+          </DivDif>
         </Item>
       </List>
     </div>
