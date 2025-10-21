8449d8e8bb27c13f3f742f6e70f5a1e03f3c3263
mark-ua-top <mark.d.nahorniak@gmail.com>
2025-10-21 16:40:41 +0300

    add-3-any-eatennnnnnn

--- src/App.js
+++ src/App.js
@@ -113,6 +113,72 @@ function App() {
             </ListDif>
           </DivDif>
         </Item>
+
+
+        <Item>
+          <Image src="https://assets.bonappetit.com/photos/62b4926736e0c90647da6b71/1:1/w_2560%2Cc_limit/0623-diana-yen-tomato-recipe-lede.jpg"
+            alt="Salat" />
+          <Title>Tomatoes With Creamy Feta</Title>
+          <DivParam>
+            <List>
+              <Item><Des>15 min</Des></Item>
+              <Item><Des>3 servings</Des></Item>
+              <Item><Des>600 calories</Des></Item>
+            </List>
+          </DivParam>
+          <DivDif>
+            <SubTitle>Difficulty</SubTitle>
+            <ListDif>
+              <ItemDifActive><DesActive>Easy</DesActive></ItemDifActive>
+              <ItemDif><Des>Medium</Des></ItemDif>
+              <ItemDif><Des>Hard</Des></ItemDif>
+            </ListDif>
+          </DivDif>
+        </Item>
+
+
+        <Item>
+          <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYt-paZcQ6FUFDsXBSN8xcl7_tkiDxbTp-Cg&s"
+            alt="salad" />
+          <Title>Spicy potato salad</Title>
+          <DivParam>
+            <List>
+              <Item><Des>30 min</Des></Item>
+              <Item><Des>2 servings</Des></Item>
+              <Item><Des>320 calories</Des></Item>
+            </List>
+          </DivParam>
+          <DivDif>
+            <SubTitle>Difficulty</SubTitle>
+            <ListDif>
+              <ItemDif><Des>Easy</Des></ItemDif>
+              <ItemDifActive><DesActive>Medium</DesActive></ItemDifActive>
+              <ItemDif><Des>Hard</Des></ItemDif>
+            </ListDif>
+          </DivDif>
+        </Item>
+
+
+        <Item>
+          <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUkFl4-e90gihmLEM7TZZ-LTbwkWQ47VaLMQ&s"
+            alt="burger" />
+          <Title>Chiken Biryani</Title>
+          <DivParam>
+            <List>
+              <Item><Des>40 min</Des></Item>
+              <Item><Des>4 servings</Des></Item>
+              <Item><Des>700 calories</Des></Item>
+            </List>
+          </DivParam>
+          <DivDif>
+            <SubTitle>Difficulty</SubTitle>
+            <ListDif>
+              <ItemDif><Des>Easy</Des></ItemDif>
+              <ItemDif><Des>Medium</Des></ItemDif>
+              <ItemDifActive><DesActive>Hard</DesActive></ItemDifActive>
+            </ListDif>
+          </DivDif>
+        </Item>
       </List>
     </div>
   );
